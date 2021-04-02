a29fc4efceb6542463d41be12aaa46055d4c93de
thakursaloni1999 <81309225+thakursaloni1999@users.noreply.github.com>
2021-04-03 00:52:30 +0530

    Add files via upload

---
+++ frontend/login_id.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2021 at 11:35 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_id`
+--
+
+CREATE TABLE `login_id` (
+  `email` varchar(20) NOT NULL,
+  `password` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login_id`
+--
+
+INSERT INTO `login_id` (`email`, `password`) VALUES
+('priyanka@gmail.com', '987654321'),
+('ravi@gmail.com', '123456789'),
+('riddhi@gmail.com', '123456789'),
+('saloni@gmail.com', 'qwerty123');
+
+--
+-- Indexes for dumped tables
+--
+--
+-- Indexes for table `login_id`
+--
+ALTER TABLE `login_id`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
